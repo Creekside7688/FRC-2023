a94da3772f065c532c188e48eb4952e47fd97098
Ryutaro623 <ryutarouonozawa@icloud.com>
2023-01-22 23:32:15 -0800

    add some comments

--- Test/src/main/java/frc/robot/commands/driveJoystick.java
+++ Test/src/main/java/frc/robot/commands/driveJoystick.java
@@ -4,52 +4,25 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.driveTrain;
 
 public class driveJoystick extends CommandBase {
-  private final driveTrain m_DriveTrain;
-  private final Joystick joystick = new Joystick(OperatorConstants.JOYSTICK_PORT);
-
   /** Creates a new driveJoystick. */
-  public driveJoystick(driveTrain d) {
-    m_DriveTrain = d;
-    addRequirements(m_DriveTrain);
-
+  public driveJoystick() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-     /*
-     * m_DriveTrain.reset_encoders();
-     * m_DriveTrain.reset_Yaw();;
-     */
-    
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    m_DriveTrain.arcadeDrive(joystick.getRawAxis(OperatorConstants.LEFT_Y_AXIS), joystick.getRawAxis(OperatorConstants.RIGHT_X_AXIS));
-    /*
-     * System.out.print("Left cm: "+m_DriveTrain.getLeft_enc_dis());
-     * System.out.print("Right cm: " +m_DriveTrain.getRight_enc_dis());
-     * System.out.print("Gyro yaw: "+m_DriveTrain.get_Yaw());
-     * System.out.print("Gyro pitch: "+ m_DriveTrain.get_Pitch());
-     * 
-     * 
-     */
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_DriveTrain.Stop();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
