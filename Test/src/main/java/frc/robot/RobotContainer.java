a2ad4e59cad9b2b97ae0348e9b5183ad48f2b4f1
Ryutaro623 <ryutarouonozawa@icloud.com>
2023-01-23 10:06:11 -0800

    added constant

--- Test/src/main/java/frc/robot/RobotContainer.java
+++ Test/src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,15 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.driveJoystick;
+import frc.robot.subsystems.driveTrain;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -23,8 +28,10 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final Joystick m_driverController =new Joystick(OperatorConstants.JOYSTICK_PORT);
+  private final Trigger rb_Button = new JoystickButton(m_driverController, OperatorConstants.RB_BUTTON);
+  private final driveTrain dt = new driveTrain();
+  private final driveJoystick drive = new driveJoystick(dt);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -43,12 +50,7 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    rb_Button.whileTrue(drive);
   }
 
   /**
--- Test/src/main/java/frc/robot/commands/driveJoystick.java
+++ Test/src/main/java/frc/robot/commands/driveJoystick.java
@@ -4,25 +4,52 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.driveTrain;
 
 public class driveJoystick extends CommandBase {
+  private final driveTrain m_DriveTrain;
+  private final Joystick joystick = new Joystick(OperatorConstants.JOYSTICK_PORT);
+
   /** Creates a new driveJoystick. */
-  public driveJoystick() {
+  public driveJoystick(driveTrain d) {
+    m_DriveTrain = d;
+    addRequirements(m_DriveTrain);
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+     /*
+     * m_DriveTrain.reset_encoders();
+     * m_DriveTrain.reset_Yaw();;
+     */
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_DriveTrain.arcadeDrive(joystick.getRawAxis(OperatorConstants.LEFT_Y_AXIS), joystick.getRawAxis(OperatorConstants.RIGHT_X_AXIS));
+    /*
+     * System.out.print("Left cm: "+m_DriveTrain.getLeft_enc_dis());
+     * System.out.print("Right cm: " +m_DriveTrain.getRight_enc_dis());
+     * System.out.print("Gyro yaw: "+m_DriveTrain.get_Yaw());
+     * System.out.print("Gyro pitch: "+ m_DriveTrain.get_Pitch());
+     * 
+     * 
+     */
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_DriveTrain.Stop();
+  }
 
   // Returns true when the command should end.
   @Override
