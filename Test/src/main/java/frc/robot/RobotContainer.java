4141866b963a7029cfa6ee3d587a30dc1fa18137
Zylve <70604613+Zylve@users.noreply.github.com>
2023-02-28 12:13:52 -0800

    test

--- Test/src/main/java/frc/robot/RobotContainer.java
+++ Test/src/main/java/frc/robot/RobotContainer.java
@@ -98,9 +98,8 @@ public class RobotContainer {
         lt_Trigger.whileTrue(Commands.run(() -> moveArm.setTargetAngle(moveArm.getTargetAngle() + ArmConstants.MANUAL_DEGREES_MOVEMENT_PER_SECOND)));
 
         // USE X AND Y BUTTONS TO TEST COMMANDS
-        x_Button.onTrue(openarm.andThen(levelWrist));
-        y_Button.onTrue(closeArm);
-
+        x_Button.onTrue(testArm);
+        y_Button.whileTrue(closeArm);
     }
 
     /**
